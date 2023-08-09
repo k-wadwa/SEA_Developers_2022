d4944ef6a8c4eb7c0360fb89735035ac1b070fa4
k-wadwa <kamal_wadwa@hotmail.com>
2023-08-09 11:14:35 +0100

    upto task 3

---
+++ ReactJSQuickLabs/a-react-app/starter/src/AnotherComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AnotherComponent = () => {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quidem.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quidem.</p>
+        </>
+    )
+}
+
+export default AnotherComponent;
